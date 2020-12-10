d7602331249de0623596a6d56958e51df473f681
Khuzama Shahid <skhuzamashahid@gmail.com>
2020-12-10 19:20:16 +0500

    updated project

--- src/index.js
+++ src/index.js
@@ -2,58 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './store/reducers/rootReducer'
-import { Provider, useSelector } from 'react-redux'
-import thunk from 'redux-thunk'
-import { reduxFirestore, getFirestore, createFirestoreInstance } from 'redux-firestore';
-import { ReactReduxFirebaseProvider, getFirebase, isLoaded } from 'react-redux-firebase'
-import firebaseConfig from './config/firebaseConfig'
-import firebase from 'firebase/app';
-import loader from './assets/images/loader.gif';
-import './assets/custom.css'
-
-const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
-        reduxFirestore(firebase, firebaseConfig)
-    )
-);
-
-const rrfConfig = {
-    userProfile: 'users',
-    useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-}
-
-
-const rrfProps = {
-    firebase,
-    config: { ...firebaseConfig, ...rrfConfig },
-    dispatch: store.dispatch,
-    createFirestoreInstance
-};
-
-function AuthIsLoaded({ children }) {
-    const auth = useSelector(state => state.firebase.auth)
-    if (!isLoaded(auth)) return <div className="splash">
-        <img src={loader} alt="" />
-    </div>;
-    return children
-}
-
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ReactReduxFirebaseProvider {...rrfProps}>
-            <AuthIsLoaded>
-                <App />
-            </AuthIsLoaded>
-        </ReactReduxFirebaseProvider>
-    </Provider>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-
-serviceWorker.register();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
